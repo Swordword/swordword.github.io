bfda2314bbc623834523bad9dbaf2caf212db02d
John Massive <massivejohn123@gmail.com>
2020-09-22 19:14:30 +0800

    fp

---
+++ pages/posts/first-post.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <h1>First aa</h1>
+    </Layout>
+  );
+}

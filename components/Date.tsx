63f4e1516d7eb60b24f6938a1c2ce57103099244
John Massive <massivejohn123@gmail.com>
2021-01-18 18:11:32 +0800

    optimization html 添加lang

--- components/Date.tsx
+++ components/Date.tsx
@@ -1,8 +1,7 @@
 import { format, parseISO } from "date-fns";
 import deLocale from "date-fns/locale/zh-CN";
 export default function Date({ dateString }: { dateString: string }) {
-  console.log("dateString", dateString);
-  if (!dateString) return <span>no time </span>;
+  if (!dateString) return <span>time is unfounded </span>;
   const date = parseISO(dateString);
   return (
     <time dateTime={dateString}>

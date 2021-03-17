97eeb82f7ed500824acbed087c625b305e8a1de7
John Massive <massivejohn123@gmail.com>
2021-03-17 09:59:28 +0800

    添加博客统计

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,19 @@ import "../styles/global.css";
 import "highlight.js/scss/vs2015.scss";
 import { ThemeContext, themes } from '../config/theme'
 
+
+function statistics() {
+  var _hmt = _hmt || [];
+  (function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?8ad71e08e3dca52439af211aea99eade";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  })();
+}
+statistics()
+
+
 export default function App({ Component, pageProps }: AppProps) {
   const [theme, setTheme] = React.useState(themes.light)
   const toggleTheme = () => {

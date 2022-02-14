ab0e52c43b4f293ec864e66ee050385a2f46a7c1
Swordword <massivejohn123@gmail.com>
2022-02-14 13:01:39 +0800

    Update [id].tsx

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,12 +1,16 @@
 // Package
 import { useContext } from 'react'
-import Head from 'next/head';
-import { GetStaticPaths, InferGetStaticPropsType, GetStaticPropsContext } from 'next';
+import Head from 'next/head'
+import {
+  GetStaticPaths,
+  InferGetStaticPropsType,
+  GetStaticPropsContext,
+} from 'next'
 import { css } from '@emotion/react'
 
 // Local
-import Layout from 'layout';
-import { getAllPostIds, getPostData } from 'lib/posts';
+import Layout from 'layout'
+import { getAllPostIds, getPostData } from 'lib/posts'
 
 import { ThemeContext } from 'config/theme'
 import Label from 'components/Label'
@@ -18,39 +22,48 @@ const Post = ({ postData }: InferGetStaticPropsType<typeof getStaticProps>) => {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <article style={{
-        color: theme.foreground,
-      }}
+      <article
+        style={{
+          color: theme.foreground,
+        }}
       >
-        <h1 css={css`
-        height:70px;
-        margin: 0;
-        line-height:70px;
-        text-align:center;
-        font-size: 40px;
-        `}
+        <h1
+          css={css`
+            height: 70px;
+            margin: 0;
+            line-height: 70px;
+            text-align: center;
+            font-size: 40px;
+          `}
         >
           {postData.title}
         </h1>
-        <div css={css`
-          margin-left: 250px;
+        <div
+          css={css`
+            margin-left: 250px;
           `}
         >
           {/* Label Component */}
-          <Label category={postData.category} tag={postData.tag} date={postData.date} />
+          <Label
+            category={postData.category}
+            tag={postData.tag}
+            date={postData.date}
+          />
         </div>
-        <div css={css`
-            border-bottom: 1px solid #30A9DE;
+        <div
+          css={css`
+            border-bottom: 1px solid #30a9de;
             margin-top: 20px;
-            margin-bottom: 20px;`}
+            margin-bottom: 20px;
+          `}
         >
-
           {/* line */}
         </div>
+        {/* eslint-disable-next-line react/no-danger */}
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
-  );
+  )
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -61,7 +74,9 @@ export const getStaticPaths: GetStaticPaths = async () => {
   }
 }
 
-export const getStaticProps = async ({ params }: GetStaticPropsContext<any>) => {
+export const getStaticProps = async ({
+  params,
+}: GetStaticPropsContext<any>) => {
   const postData = await getPostData(params.id as string)
   return {
     props: {

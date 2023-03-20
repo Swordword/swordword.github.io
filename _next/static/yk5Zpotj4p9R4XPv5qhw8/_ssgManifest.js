8951ec5f8f87e2faaa074d479ce0fbcd741dc53d
Swordword <Swordword@users.noreply.github.com>
2023-03-20 10:09:54 +0000

    deploy: 09ca024117c936ed31d39a268aa7dbd1a8cdbed0

---
+++ _next/static/yk5Zpotj4p9R4XPv5qhw8/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Farchive","\u002Fcategory","\u002Fcategory\u002F[cate]","\u002Fposts\u002F[id]","\u002Ftag","\u002Ftag\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
